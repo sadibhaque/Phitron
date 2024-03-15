d210e50db3f6a4b67d91f3717d4bd53c77eae9e6
Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-15 15:27:50 +0600

    Create G - Not Found.cpp

---
+++ Week_1/G - Not Found.cpp
@@ -0,0 +1,87 @@
+/* Each conquered bug is a step towards mastery. */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+typedef pair<int,int> pi;
+typedef pair<string,int> ps;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<ps> vps;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n";
+#define psum cout<<sum<<"\n";
+#define pans cout<<ans<<"\n";
+#define ptmp cout<<tmp<<"\n";
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define sortv(x) sort((x).begin(),(x).end())
+#define sorta(x,n) sort((x),(x)+(n))
+#define revv(x) reverse((x).begin(),(x).end())
+#define reva(x,n) reverse((x),(x)+(n))
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+    string str;
+    cin>>str;
+
+    sortv(str);
+
+    set <char> s;
+    for(auto it : str){
+        s.ins(it);
+    }
+
+    char ch = 'a';
+
+    while(!s.empty()){
+        char tmp = *s.begin();
+
+        if(tmp == ch){
+            ch++;
+            s.erase(s.begin());
+        }
+        else{
+            if(!s.empty()){
+                cout<<ch<<'\n';
+                return;
+            }
+        }
+    }
+
+    if(ch > 'z')cout<<"None\n";
+    else cout<<ch<<"\n";
+}
+
+int sadib(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
