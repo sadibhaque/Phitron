5ddc2eaf0111512281c3e11eaf27aa46bce7ce69
Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-15 16:06:15 +0600

    1

---
+++ Week_1/H - How many?.cpp
@@ -0,0 +1,74 @@
+/* Each conquered bug is a step towards mastery. */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+typedef pair<int,int> pi;
+typedef pair<string,int> ps;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<ps> vps;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n";
+#define psum cout<<sum<<"\n";
+#define pans cout<<ans<<"\n";
+#define ptmp cout<<tmp<<"\n";
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define sortv(x) sort((x).begin(),(x).end())
+#define sorta(x,n) sort((x),(x)+(n))
+#define revv(x) reverse((x).begin(),(x).end())
+#define reva(x,n) reverse((x),(x)+(n))
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+
+    int ctr = 0;
+
+    for(int i = 0; i <= a; i++){
+        for(int j = 0; j <= a; j++){
+            for(int k = 0; k <= a; k++){
+                if((i+j+k <= a) && (i*j*k <= b)){
+                    ctr++;
+                }
+            }
+        }
+    }
+
+    pctr;
+}
+
+int sadib(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
