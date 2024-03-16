0a1694c09dd0d06afdbd56206abbd89c54a93c6e
Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-16 19:00:18 +0600

    Create A. Wet Shark and Odd and Even.cpp

---
+++ Week_1/Day_2/A. Wet Shark and Odd and Even.cpp
@@ -0,0 +1,77 @@
+/* Each conquered bug is a step towards mastery. */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+
+typedef pair<int,int> pi;
+typedef pair<string,int> ps;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<ps> vps;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n";
+#define psum cout<<sum<<"\n";
+#define pans cout<<ans<<"\n";
+#define ptmp cout<<tmp<<"\n";
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define sortv(x) sort((x).begin(),(x).end())
+#define sorta(x,n) sort((x),(x)+(n))
+#define revv(x) reverse((x).begin(),(x).end())
+#define reva(x,n) reverse((x),(x)+(n))
+#define all(x) (x).begin(),(x).end()
+
+
+void solve(){
+    int n;
+    cin>>n;
+
+    vl arr(n);
+    ll sum = 0, mn = INT_MAX;
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+        sum += arr[i];
+
+        if(arr[i] % 2 != 0){
+            mn = min(mn,arr[i]);
+        }
+
+    }
+    if(sum % 2 == 0){
+        psum;
+    }
+    else cout<<sum - mn<<endl;
+}
+
+int sadib(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
