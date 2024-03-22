b97eaea7108fd980687a18cbd0a1d3e676c5fc59
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-22 04:38:49 -0700

    Create C - Remove It.cpp

---
+++ Week_2/Day_1/C - Remove It.cpp
@@ -0,0 +1,47 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n"
+#define pans cout<<ans<<"\n"
+
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+	int n,m;
+	cin>>n>>m;
+	
+	int x;
+	for(int i = 0; i < n; i++){
+		cin>>x;
+		if(x != m){
+			cout<<x<<" ";
+		}
+	}
+}
+
+int sadib(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	//cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
