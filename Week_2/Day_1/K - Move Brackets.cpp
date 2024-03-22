c520e85a7ab98abc16d046ff4f4e9a7b1509559d
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-22 06:44:28 -0700

    Create K - Move Brackets.cpp

---
+++ Week_2/Day_1/K - Move Brackets.cpp
@@ -0,0 +1,57 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n"
+#define pans cout<<ans<<"\n"
+
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+	int n;
+	cin>>n;
+	string str;
+	cin>>str;
+	
+	stack <char> stk;
+	
+	int ctr = 0;
+	
+	for(int i = 0; i < n; i++){
+		if(str[i] == '('){
+			stk.push(str[i]);
+		}
+		else if(!stk.empty() && str[i] == ')'){
+			stk.pop();
+		}
+		else ctr++;
+	}
+	
+	pctr;
+}
+
+int sadib(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
