b4e73b6909db881f56f9e189fbcf9c6eaa9a880b
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-22 10:09:41 -0700

    Create Segment with Small Sum.cpp

---
+++ Week_2/Topicwise/Segment with Small Sum.cpp
@@ -0,0 +1,64 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pctr cout<<ctr<<"\n"
+#define pans cout<<ans<<"\n"
+
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+  int n; 
+  ll sum;
+  cin>>n>>sum;
+
+  vi arr(n);
+  for(int i = 0; i < n; i++){
+    cin>>arr[i];
+  }
+
+  ll tmp = 0;
+  int l = 0, r = 0, ans = 0, ctr = 0;
+
+  while(r < n){
+    tmp += arr[r];
+    if(tmp <= sum){
+      ctr++;
+      ans = max(ans,r - l + 1);
+    }
+    else{
+      tmp -= arr[l];
+	  l++;
+	  ctr--;
+    }
+	r++;
+  }
+
+  pans;
+}
+
+int sadib(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  //cin>>t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
