797a400a148cbf9bd6afa94864f9d8a6dc96c7ea
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-03-30 13:52:47 -0700

    Created F - Line.cpp

--- Week_3/Day_2/F - Line.cpp
+++ Week_3/Day_2/F - Line.cpp
@@ -1,1 +1,57 @@
-//
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define sadib main
+#define all(x) (x).begin(),(x).end()
+
+void solve(){
+	int n;
+	cin>>n;
+	string str;
+	cin>>str;
+	
+	ll mx = 0;
+	vl arr;
+	for(int i = 0; i < n; i++){
+		if(str[i] == 'L'){
+			mx += i;
+			arr.pb((n-1-i)-i);
+		}
+		else{
+			mx += (n - i - 1);
+			arr.pb(i-(n-1-i));
+		}
+	}
+	sort(all(arr),greater<ll>());
+	
+	for(int i = 0; i < n; i++){
+		if(arr[i] > 0) mx += arr[i];
+		cout<<mx<<" ";
+	}
+	pend;
+}
+
+int sadib(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
