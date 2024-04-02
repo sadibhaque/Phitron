847f988a5357cf6f14275a79bdcab5424cf69046
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-02 08:36:53 +0600

    Create R - Remove Prefix.cpp

---
+++ Week_3/Day_5/R - Remove Prefix.cpp
@@ -0,0 +1,53 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+void solve();
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define all(x) (x).begin(),(x).end()
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+
+void solve(){
+	int n;
+	cin>>n;
+	
+	vi arr(n);
+	map <int,int> mp;
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+	}
+	
+	int ans = 0;
+	
+	for(int i = n-1; i >= 0; i--){
+		if(mp[arr[i]]){
+			ans = i+1;
+			break;
+		}
+		else mp[arr[i]]++;
+	}
+	
+	pans;
+}
