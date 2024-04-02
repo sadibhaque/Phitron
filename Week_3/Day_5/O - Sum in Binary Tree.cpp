4ccc8797c97a1f3f9f99bc8dde166b247c9be3df
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-02 06:59:22 +0600

    Create O - Sum in Binary Tree.cpp

---
+++ Week_3/Day_5/O - Sum in Binary Tree.cpp
@@ -0,0 +1,46 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+void solve();
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define all(x) (x).begin(),(x).end()
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+
+void solve(){
+	ll n;
+	cin>>n;
+	
+	ll ans = 0;
+	
+	while(n > 1){
+		ans += n;
+		n /= 2;
+	}
+	
+	ans++;
+	
+	pans;
+}
