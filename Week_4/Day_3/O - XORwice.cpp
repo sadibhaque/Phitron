9a450ff98255886b4d9857df3f722321c40ca865
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-07 16:47:48 +0600

    Create O - XORwice.cpp

---
+++ Week_4/Day_3/O - XORwice.cpp
@@ -0,0 +1,43 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+void solve();
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define f first
+#define s second
+#define ins insert
+#define all(x) (x).begin(),(x).end()
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen ("input.txt","r",stdin);
+	#endif
+	int t = 1;
+	cin>>t;
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+
+void solve(){
+	int a,b;
+	cin>>a>>b;
+	
+	ll ans = min((a^b) , a+b);
+	
+	pans;
+}
+
