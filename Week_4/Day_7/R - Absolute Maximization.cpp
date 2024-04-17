bbb498b0c9b8ca8e496325f7d2642f504fdcd729
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-17 06:24:27 -0700

    Create R - Absolute Maximization.cpp

---
+++ Week_4/Day_7/R - Absolute Maximization.cpp
@@ -0,0 +1,50 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------START-----------------------*/
+
+void solve(){
+	int n;
+	cin>>n;
+	
+	vi arr(n);
+	int a = INT_MAX, b = 0;
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+		a &= arr[i];
+		b |= arr[i];
+	}
+	int ans = abs(b - a);
+	pans;
+}
+
+void testcase(){
+	int t = 1;
+	cin>>t;
+	while(t--){solve();}
+}
+
+/*----------------------END------------------------*/
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	#endif
+	testcase();
+	return 0;
+}
