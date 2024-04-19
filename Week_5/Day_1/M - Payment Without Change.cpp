a85309c616a5753b20c730b316b20ff2f1192578
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-19 11:07:19 -0700

    Create M - Payment Without Change.cpp

---
+++ Week_5/Day_1/M - Payment Without Change.cpp
@@ -0,0 +1,44 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------START-----------------------*/
+
+void solve(){
+	ll a,b,n,sum;
+	cin>>a>>b>>n>>sum;
+	
+	 if(sum % n <= b && (a * n) + b >= sum) py;
+	 else pn;
+}
+
+void testcase(){
+	int t = 1;
+	cin>>t;
+	while(t--){solve();}
+}
+
+/*----------------------END------------------------*/
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	#endif
+	testcase();
+	return 0;
+}
+
