40c0e1448791829c3840859b56fea6723923e1c7
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-20 06:21:52 -0700

    Create H - Negatives and Positives.cpp

---
+++ Week_5/Day_2/H - Negatives and Positives.cpp
@@ -0,0 +1,65 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------START-----------------------*/
+
+void solve(){
+	int n;
+	cin>>n;
+	
+	vl arr(n);
+	ll sum = 0, neg = 0, mn = INT_MAX;
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+		sum += arr[i];
+		if(arr[i] < 0) neg++;
+		mn = min(mn,abs(arr[i]));
+	}
+	
+	if(neg % 2 == 0){
+		ll ans = 0;
+		for(int i = 0; i < n; i++){
+			ans += abs(arr[i]);
+		}
+		pans;
+	}
+	else{
+		ll ans = 0;
+		for(int i = 0; i < n; i++){
+			ans += abs(arr[i]);
+		}
+		ans -= abs(mn) * 2;
+		pans;
+	}
+}
+
+void testcase(){
+	int t = 1;
+	cin>>t;
+	while(t--){solve();}
+}
+
+/*----------------------END------------------------*/
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	#endif
+	testcase();
+	return 0;
+}
