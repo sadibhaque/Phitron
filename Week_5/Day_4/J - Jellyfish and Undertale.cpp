8013691a700f7e94dcc3458032afeded9afb862b
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-04-23 13:33:13 +0600

    Create J - Jellyfish and Undertale.cpp

---
+++ Week_5/Day_4/J - Jellyfish and Undertale.cpp
@@ -0,0 +1,49 @@
+/* Each conquered bug is a step towards mastery. */
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+#define pend cout<<"\n"
+#define pans cout<<ans<<"\n"
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+
+/*---------------------START-----------------------*/
+
+void solve(){
+	ll a,b,n;
+	cin>>a>>b>>n;
+	
+	vl arr(n);
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+		b += min(arr[i],a-1);
+	}
+	
+	cout<<b<<endl;
+}
+
+void testcase(){
+	int t = 1;
+	cin>>t;
+	while(t--){solve();}
+}
+
+/*----------------------END------------------------*/
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	#endif
+	testcase();
+	return 0;
+}
+
