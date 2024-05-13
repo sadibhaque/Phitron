e3e43e03a7f4fff623a97ecd5f745b1d73882736
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-05-13 22:19:19 +0600

    Create N - MEX Repetition.cpp

---
+++ Week_7/Day_3/N - MEX Repetition.cpp
@@ -0,0 +1,46 @@
+/*
+ ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+ ░▒▓██████▓▒░ ░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+*/
+
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> arr(n + 1);
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		sum ^= i;
+		sum ^= arr[i];
+	}
+
+	sum ^= n;
+	arr[n] = sum;
+
+	rotate(arr.begin(), arr.begin() + (n + 1 - k % (n + 1)) % (n + 1), arr.end());
+
+
+	for (int i = 0; i < n; i++) cout << arr[i] << " ";
+	cout << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+}
