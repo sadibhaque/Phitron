569c5e69db9fe9c0e49bf8450a4161f51362fee0
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-05-13 22:32:44 +0600

    Create P - YetnotherrokenKeoard.cpp

---
+++ Week_7/Day_3/P - YetnotherrokenKeoard.cpp
@@ -0,0 +1,54 @@
+/*
+ ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+ ░▒▓██████▓▒░ ░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+*/
+
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+	string str;
+	cin >> str;
+
+	int n = str.size();
+	stack<int> u, l;
+	for (int i = 0; i < n; i++) {
+		if (str[i] == 'b') {
+			if (!l.empty()) {
+				str[l.top()] = '#';
+				l.pop();
+			}
+			str[i] = '#';
+		}
+		else if (str[i] == 'B') {
+			if (!u.empty()) {
+				str[u.top()] = '#';
+				u.pop();
+			}
+			str[i] = '#';
+		}
+		else if (islower(str[i])) l.push(i);
+		else u.push(i);
+	}
+	for (int i = 0; i < n; i++) {
+		if (str[i] != '#') cout << str[i];
+	}
+	cout << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+}
