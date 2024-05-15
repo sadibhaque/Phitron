154d12fa14f886692f21614806f608e756412f33
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-05-15 15:58:48 +0600

    Create V - GCD Partition.cpp

---
+++ Week_8/Day_3/V - GCD Partition.cpp
@@ -0,0 +1,47 @@
+/*
+ ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+ ░▒▓██████▓▒░ ░▒▓████████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+       ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓███████▓▒░
+*/
+
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vector <long long int> arr(n);
+	long long   sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
+
+	long long tmp = 0, ans = 0;
+
+	for (int i = 0; i < n - 1; i++) {
+		sum -= arr[i];
+		tmp += arr[i];
+
+		ans = max(ans, __gcd(sum, tmp));
+	}
+
+	cout << ans << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+}
