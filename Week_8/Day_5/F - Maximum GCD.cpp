8d4e73634e129516d33da6af091d909991022bbf
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-05-18 14:58:23 -0700

    Create F - Maximum GCD.cpp

---
+++ Week_8/Day_5/F - Maximum GCD.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+	long long n;
+	cin >> n;
+
+	cout << n / 2 << endl;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+}
