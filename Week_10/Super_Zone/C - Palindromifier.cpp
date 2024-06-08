cdbcd3c1a2d7e2f19c077968e346faabe500937b
Sadiqul Haque Sadib <111662359+sadibhaque@users.noreply.github.com>
2024-06-08 18:19:35 +0600

    Create C - Palindromifier.cpp

---
+++ Week_10/Super_Zone/C - Palindromifier.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef MENDAX
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+
+#define int long long
+#define lcm(a, b) ((a * b) / __gcd(a, b));
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void S_1984() {
+    string str;
+    cin >> str;
+
+    int n = str.length();
+
+    cout << 3 << endl;
+    cout << "R " << n-1 << endl;
+    cout << "L " << n << endl;
+    cout << "L 2\n";
+}
+
+signed main() {
+#ifndef MENDAX
+    ios_base::sync_with_stdio(0); cin.tie(0);
+#endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    S_1984();
+    return 0;
+}
